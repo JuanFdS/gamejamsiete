e4701689cdba4723502322e53af38fe0bde52031
emanuel.suriano@fdvsolutions.com <emanuel.suriano@fdvsolutions.com>
2017-01-21 17:25:22 -0300

    Create obstaculoContrario

---
+++ Assets/Scripts/ObstaculoContrario.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaculoContrario : IObstaculo
+{
+    private float initialYPosition;
+    private float timeAlive;
+
+    override public void BeAffectedBy(EffectArea specialPower)
+    {
+        Destroy(this.gameObject);
+    }
+
+    private float YPosition()
+    {
+        return (Mathf.Sin(timeAlive * 9)) / 2;
+    }
+
+    private float XPosition()
+    {
+        return transform.position.x - Time.deltaTime * 5;
+    }
+    
+    void Start()
+    {
+        timeAlive = 0;
+        initialYPosition = transform.position.y;
+    }
+
+    void Update()
+    {
+        timeAlive += Time.deltaTime;
+        transform.position = new Vector3(XPosition(), initialYPosition + YPosition(), transform.position.z);
+    }
+}
