7a55724f20c8211760c306b4124d97e8cab87d18
Francisco Hart <fhart91@gmail.com>
2017-01-21 00:51:28 -0300

    Updated Repo

---
+++ Assets/Scripts/GlobalConfig.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GlobalConfig : MonoBehaviour
+{
+    public static GlobalConfig Instance;
+
+    public float[] lines;
+
+	void Start ()
+    {
+	    if (Instance != null)
+	    {
+	        Destroy(gameObject);
+	    }
+
+	    Instance = this;
+    }
+}
--- Assets/Scripts/Pulpito.cs
+++ Assets/Scripts/Pulpito.cs
@@ -2,29 +2,75 @@
 
 public class Pulpito : MonoBehaviour
 {
-    public float speed;
+    public float horizontalSpeed;
+    public float verticalSpeed;
 
-	void Update ()
+    public int currentLine = 3;
+
+    private Vector3 lastPosition;
+    private Vector3 nextPosition;
+
+    private float stepTime;
+    private bool stepping;
+
+    public void MoveVertically()
     {
-        transform.position += Vector3.right * speed * Time.deltaTime;
+        if (stepping)
+        {
+            stepTime = Mathf.Clamp01(stepTime + verticalSpeed * Time.deltaTime);
+            transform.position = Vector3.Lerp(lastPosition, nextPosition, stepTime);
+
+            if (stepTime >= 1)
+            {
+                stepping = false;
+            }
+
+            return;
+        }
+
+        lastPosition = transform.position;
+        stepTime = 0;
 
         if (Input.GetButton("Red"))
         {
-            transform.position += Vector3.down * speed * Time.deltaTime;
+            if (transform.position.y <= GlobalConfig.Instance.lines[6])
+            {
+                return;
+            }
+
+            currentLine++;
+            nextPosition = new Vector3(lastPosition.x, GlobalConfig.Instance.lines[currentLine], 0);
+            stepping = true;
         }
 
         if (Input.GetButton("Blue"))
         {
-            transform.position += Vector3.up * speed * Time.deltaTime;
+            if (transform.position.y >= GlobalConfig.Instance.lines[0])
+            {
+                return;
+            }
+
+            currentLine--;
+            nextPosition = new Vector3(lastPosition.x, GlobalConfig.Instance.lines[currentLine], 0);
+            stepping = true;
         }
     }
 
-  void OnTriggerEnter2D(Collider2D collisioner){
-    switch(collisioner.gameObject.tag){
-      case "Obstacle": {
-        Debug.Log("Colisionó");
-        break;
-      }
+    public void MoveHorizontally()
+    {
+        transform.position += Vector3.right * horizontalSpeed * Time.deltaTime;
+        lastPosition += Vector3.right * horizontalSpeed * Time.deltaTime;
+        nextPosition += Vector3.right * horizontalSpeed * Time.deltaTime;
     }
-  }
+
+    public void Move()
+    {
+        MoveVertically();
+        MoveHorizontally();
+    }
+
+    public void Update()
+    {
+        Move();
+    }
 }
