1408a2b56dacbd6eb3e7a4812555a718dadebc6a
Francisco Hart <fhart91@gmail.com>
2017-01-21 16:54:08 -0300

    Fonts folder

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnExit()
+    {
+        Application.Quit();
+    }
+}
