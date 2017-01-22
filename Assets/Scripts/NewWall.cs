dbcc6917d2b660c4b7da4998cfc55b917bde4e0a
emanuel.suriano@fdvsolutions.com <emanuel.suriano@fdvsolutions.com>
2017-01-22 13:43:28 -0300

    change generator

---
+++ Assets/Scripts/NewWall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewWall : ObstaculoEstatico
+{
+    public float posY;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = new Vector3(transform.position.x, posY, transform.position.z);
+	}
+}
